b8c4f2a5f8f3261ac71cf7a66cc95b3d19a56041
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-03-25 01:47:32 +0200

    add basic test

--- test/test_spec.js
+++ test/test_spec.js
@@ -4,6 +4,10 @@ describe('Protractor demo', () => {
         browser.ignoreSynchronization = true
         browser.get('http://duckduckgo.com')
 
+        element(by.id('search_form_input_homepage')).sendKeys('Lorum ipsum')
+        element(by.id('search_button_homepage')).click()
+
+        expect(element(by.className('module__body js-about-item')).isPresent()).toBe(true)
         browser.sleep(5000)
     })
 })
